35b85b16b8557dca42dd04d1657d6276ee6e9f83
phoebeTree <40566927+phoebemoon16@users.noreply.github.com>
2019-07-04 20:33:31 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import VueRouter from 'vue-router';
+import "./config/axios"
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
