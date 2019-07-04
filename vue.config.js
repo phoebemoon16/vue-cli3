12203a5625953332402ccd4960ba77200ef17603
phoebeTree <40566927+phoebemoon16@users.noreply.github.com>
2019-07-04 20:36:11 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,6 @@ module.exports = {
     devServer: {
       proxy: {
         "/": {
-          // 育权
-          // target: "http://192.168.10.76:8090",
-          // 董俊
           target: "http://192.168.10.76:8090",
           ws: false,
           changeOrigin: true,
